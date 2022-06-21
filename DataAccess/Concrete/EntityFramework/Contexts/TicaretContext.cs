86ff98655b40f1fbc7b1b768f162834e4b7ee28c
Mehmet Ali TOPRAK <92664474+toprakmehmetali@users.noreply.github.com>
2022-06-21 21:04:06 +0300

    test

--- DataAccess/Concrete/EntityFramework/Contexts/TicaretContext.cs
+++ DataAccess/Concrete/EntityFramework/Contexts/TicaretContext.cs
@@ -17,10 +17,8 @@ namespace DataAccess.Concrete.EntityFramework.Contexts
         public DbSet<OperationClaim> OperationClaims { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
-
         public DbSet<Basket> Baskets { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-
     }
 }
